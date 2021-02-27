24950a7065b6bccdc0ec409713f64ed1fa503abc
Matheus Ferro <matheusferroraimundo@gmail.com>
2021-02-27 19:19:53 -0300

    feat: countdown, botao abandonar e ciclo encerrado

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,9 +3,28 @@ import { useState, useEffect } from 'react'
 
 // useEffect -> Efeitos colaterais, quando algo acontecer executa algo ( dispara algum efeito colateral )
 
+let countdownTimeout: NodeJS.Timeout
+
+/*
+
+  // if then
+  { isActive && ( ) }
+
+  // if else
+  { isActive ? ( ) : ( ) }
+
+  // Fragment, mesma coisa que div porém não aparece lá no html, seve para resolver o problema do (linha 20) 
+  <> </>
+
+  // if else{ if else }
+  { hasFinished ? (  ) : ( <> { isActive ?  ( ) : (  )} </> )}
+
+*/
+
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60)
-  const [active, setActive] = useState(false)
+  const [time, setTime] = useState(0.1 * 60)
+  const [isActive, setIsActive] = useState(false)
+  const [hasFinished, setHasFinished] = useState(false)
 
   const minutes = Math.floor(time / 60) // Arredondar para baixo
   const seconds = time % 60
@@ -17,21 +36,29 @@ export function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
   function startCountdown() {
-    setActive(true)
-
-
+    setIsActive(true)
   }
 
-  // 1° Paramêtro -> O que eu quero executar (sempre uma function)
+  function resetCountdown() {
+    clearTimeout(countdownTimeout) // Limpando o timeout ( ou fila do timeout )   
+    // Como o isActive vai mudar para false, vai disparar o useEffect novamente ( efeito colateral )
+    setIsActive(false)
+    setTime(0.1 * 60)  
+  }
+
+  // 1° Paramêtro -> O que eu quero executar (sempre uma function) 
   // 2° Paramêtro -> Quando quero executar
   useEffect(() => {
-    if(active && time > 0){
-      setTimeout(() => {
+    if(isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time-1)
       }, 1000) // A cada 1 segundo diminui 1 no Time
+    } else if(isActive && time == 0) {
+      setHasFinished(true)
+      setIsActive(false)
     }
-  }, [active, time]) // Vai executar quando o active e o time mudar, nesse caso, como o time está mudando, vai executar a função repetidamente
-
+  }, [isActive, time]) // Vai executar quando o active e o time mudar, nesse caso, como o time está mudando, vai executar a função repetidamente
+ 
   return(
     <div>
       <div className={styles.countdownContainer}> 
@@ -46,9 +73,26 @@ export function Countdown() {
         </div>
       </div>
 
-      <button type="button" className={styles.countdownButton} onClick={startCountdown}>
-        Iniciar um ciclo
-      </button>
+      
+      { hasFinished ? (
+        <button className={styles.countdownButton} disabled>
+          Ciclo encerrado
+        </button>
+      ) : (
+        <>
+          { isActive ? (
+            <button type="button" className={`${styles.countdownButton} ${styles.countdownButtonActive}`} onClick={resetCountdown}>
+              Abandonar ciclo
+            </button>
+            ) : (
+            <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
+
+ 
     </div>
   )
 }
